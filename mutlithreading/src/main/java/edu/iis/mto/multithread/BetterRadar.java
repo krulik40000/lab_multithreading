22055cf331ca9ba28bff6391f066fcf6f7f6ed1f
Kovalyk.Volodymyr <kovalykvovapl@gmail.com>
2019-05-11 10:11:07 +0200

    add components to BetterRadar and RacketPatriot

--- mutlithreading/src/main/java/edu/iis/mto/multithread/BetterRadar.java
+++ mutlithreading/src/main/java/edu/iis/mto/multithread/BetterRadar.java
@@ -1,4 +1,18 @@
 package edu.iis.mto.multithread;
 
 public class BetterRadar {
+    private PatriotBattery battery;
+    private RacketPatriot patriot;
+    private final int SALVO_SIZE;
+
+    public BetterRadar(PatriotBattery battery, RacketPatriot patriot, int rocketPackSize) {
+        this.SALVO_SIZE = rocketPackSize;
+        this.battery = battery;
+        this.patriot = patriot;
+    }
+
+    public void notice(Scud enemyMissle) {
+        this.patriot.patriotRockets(this.battery, SALVO_SIZE);
+    }
+
 }
--- mutlithreading/src/main/java/edu/iis/mto/multithread/RacketPatriot.java
+++ mutlithreading/src/main/java/edu/iis/mto/multithread/RacketPatriot.java
@@ -1,4 +1,23 @@
 package edu.iis.mto.multithread;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class RacketPatriot {
+    private boolean isMultithreaded;
+    private ExecutorService service;
+
+    public RacketPatriot(boolean multiThreaded) {
+        this.isMultithreaded = multiThreaded;
+        service = Executors.newCachedThreadPool();
+    }
+
+    void patriotRockets(PatriotBattery battery, int salvoSize) {
+        for (int i = 0; i < salvoSize; i++) {
+            if (isMultithreaded)
+                service.submit(() -> battery.launchPatriot());
+            else
+                battery.launchPatriot();
+        }
+    }
 }
