9eb2d616d7105e1ab472f2fe1efe6b17bfabfd76
Kovalyk.Volodymyr <kovalykvovapl@gmail.com>
2019-05-11 10:22:26 +0200

    add to test RepeatRule and Mock

--- mutlithreading/src/test/java/edu/iis/mto/multithread/BetterRadarTest.java
+++ mutlithreading/src/test/java/edu/iis/mto/multithread/BetterRadarTest.java
@@ -1,7 +1,18 @@
 package edu.iis.mto.multithread;
 
+import org.junit.Rule;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
 import static org.junit.Assert.*;
 
+@RunWith(MockitoJUnitRunner.class)
 public class BetterRadarTest {
 
+    @Rule
+    public RepeatRule repeatRule = new RepeatRule();
+
+    @Mock
+    PatriotBattery batteryMock;
 }
