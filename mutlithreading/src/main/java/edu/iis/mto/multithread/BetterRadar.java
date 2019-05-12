c46feb3fb6c35096172f6c0b5d969d4adf8e61e7
209278 <209278@edu.p.lodz.pl>
2019-05-12 19:13:14 +0200

    BetterRadar

---
+++ mutlithreading/src/main/java/edu/iis/mto/multithread/BetterRadar.java
@@ -0,0 +1,33 @@
+package edu.iis.mto.multithread;
+
+import javafx.scene.Scene;
+
+import java.util.concurrent.Executor;
+
+public class BetterRadar implements Runnable {
+
+    private PatriotBattery patriotBattery;
+    private int numberOfShots;
+    private Executor executor;
+
+    public BetterRadar(PatriotBattery patriotBattery, int numberOfShots, Executor executor){
+        this.patriotBattery = patriotBattery;
+        this.numberOfShots = numberOfShots;
+        this.executor = executor;
+    }
+
+    public void notice(Scud enemyMissle){
+        launchPatriot();
+    }
+
+    private void launchPatriot(){
+        executor.execute(this);
+    }
+
+    @Override
+    public void run() {
+        for(int i = 0; i < numberOfShots; i++){
+            patriotBattery.launchPatriot();
+        }
+    }
+}
