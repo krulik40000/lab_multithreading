2001ee9c2badb494e58079182d2b497850af6704
209420 <209420@edu.p.lodz.pl>
2019-06-02 11:52:19 +0200

    add test lunch_patriot_n_times_when_notice_enemy_missle_on_same_thread, and edited first test

--- mutlithreading/src/test/java/edu/iis/mto/multithread/BetterRadarTest.java
+++ mutlithreading/src/test/java/edu/iis/mto/multithread/BetterRadarTest.java
@@ -12,14 +12,28 @@ public class BetterRadarTest {
 
 
     @Test
-    public void launch_patriot_one_time_when_notice_enemy_missle_on_different_thread(){
+    public void launch_patriot_n_times_when_notice_enemy_missle_on_different_thread(){
 
+        int n = 5;
         RocketExecutor rocketExecutor = new RocketExecutor(RocketExecutor.RocketExecutorType.DIFF_THREAD);
-        BetterRadar betterRadar = new BetterRadar(battery,1,rocketExecutor);
+        BetterRadar betterRadar = new BetterRadar(battery,n,rocketExecutor);
 
         betterRadar.notice(new Scud());
 
-        Mockito.verify(battery , Mockito.times(1)).launchPatriot();
+        Mockito.verify(battery , Mockito.times(n)).launchPatriot();
+
+    }
+
+    @Test
+    public void lunch_patriot_n_times_when_notice_enemy_missle_on_same_thread(){
+
+        int n = 123;
+        RocketExecutor rocketExecutor = new RocketExecutor(RocketExecutor.RocketExecutorType.SAME_THREAD);
+        BetterRadar betterRadar = new BetterRadar(battery,n,rocketExecutor);
+
+        betterRadar.notice(new Scud());
+
+        Mockito.verify(battery , Mockito.times(n)).launchPatriot();
 
     }
 
