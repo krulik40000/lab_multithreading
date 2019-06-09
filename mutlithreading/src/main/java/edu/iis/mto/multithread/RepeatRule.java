690997d0d6c466096b5cb1d20c5ea7d954275df0
Ferdynand Felsztukier <ferdynand.felsztukier@gmail.com>
2019-06-10 00:52:18 +0200

    add launchPatriotOnceWhenNoticesAScudMissileInTheSameThread test

---
+++ mutlithreading/src/main/java/edu/iis/mto/multithread/RepeatRule.java
@@ -0,0 +1,13 @@
+package edu.iis.mto.multithread;
+
+import org.junit.rules.TestRule;
+import org.junit.runner.Description;
+import org.junit.runners.model.Statement;
+
+public class RepeatRule implements TestRule {
+
+    @Override
+    public Statement apply(Statement statement, Description description) {
+        return null;
+    }
+}
--- mutlithreading/src/test/java/edu/iis/mto/multithread/BetterRadarTest.java
+++ mutlithreading/src/test/java/edu/iis/mto/multithread/BetterRadarTest.java
@@ -1,18 +1,34 @@
 package edu.iis.mto.multithread;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.mockito.Mockito.*;
 
 public class BetterRadarTest {
 
+    private PatriotBattery batteryMock;
+
+    @Before
+    public void initialize() {
+        batteryMock = mock(PatriotBattery.class);
+    }
+
     @Test
-    public void launchPatriotOnceWhenNoticesAScudMissile() {
-        PatriotBattery batteryMock = mock(PatriotBattery.class);
+    public void launchPatriotOnceWhenNoticesAScudMissileInDifferentThread() {
         ThreadExecutor executor = new ThreadExecutor(ThreadExecutorOption.FIRE_IN_NEW_THREAD);
         BetterRadar radar = new BetterRadar(batteryMock, insignificantNumberOfMissiles(), executor);
         radar.notice(new Scud());
         verify(batteryMock, times(insignificantNumberOfMissiles())).launchPatriot();
+    }
+
+    @Test
+    public void launchPatriotOnceWhenNoticesAScudMissileInTheSameThread() {
+        ThreadExecutor executor = new ThreadExecutor(ThreadExecutorOption.FIRE_IN_SAME_THREAD);
+        BetterRadar radar = new BetterRadar(batteryMock, insignificantNumberOfMissiles(), executor);
+        radar.notice(new Scud());
+        verify(batteryMock, times(insignificantNumberOfMissiles())).launchPatriot();
+
     }
 
     private int insignificantNumberOfMissiles() {
