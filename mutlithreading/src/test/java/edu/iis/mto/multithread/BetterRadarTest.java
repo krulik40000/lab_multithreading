45a8170b255b3896ddc965b07290f105759a0cc4
krulik40000 <michalmalis@o2.pl>
2019-06-30 23:34:11 +0200

    dodana możliwość wykonywania testów wielokrotnie

--- mutlithreading/src/test/java/edu/iis/mto/multithread/BetterRadarTest.java
+++ mutlithreading/src/test/java/edu/iis/mto/multithread/BetterRadarTest.java
@@ -4,6 +4,7 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.times;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
@@ -13,11 +14,16 @@ public class BetterRadarTest {
     @Mock
     PatriotBattery patriotBattery;
     
+    @Rule
+    public RepeatRule repeatRule= new RepeatRule();
+    
     @Test
+    @RepeatRule.Repeat(times = 3)
     public void launchPatriotOnceWhenNoticesAScudMissle() {
         MockitoAnnotations.initMocks(this);
         BetterRadar betterRadar = new BetterRadar(patriotBattery,new BatteryExecutor(true),3);
         betterRadar.notice(new Scud());
+        System.out.println(".");
         verify(patriotBattery,times(3)).launchPatriot();
     }
 
