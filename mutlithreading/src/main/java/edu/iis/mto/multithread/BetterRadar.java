5459d6837f4cd9b959b791edb3577d1c81464097
krulik40000 <michalmalis@o2.pl>
2019-06-30 22:36:22 +0200

    Dodanie metod do klasy BetterRadar

--- mutlithreading/src/main/java/edu/iis/mto/multithread/BetterRadar.java
+++ mutlithreading/src/main/java/edu/iis/mto/multithread/BetterRadar.java
@@ -2,22 +2,34 @@ package edu.iis.mto.multithread;
 
 import java.util.concurrent.Executor;
 
-public class BetterRadar {
+public class BetterRadar implements Runnable {
 
     private PatriotBattery battery;
     private Executor executor;
     private int rocketAmount;
-    
+
     public BetterRadar(PatriotBattery battery, Executor executor, int rocketAmount) {
         super();
         this.battery = battery;
         this.executor = executor;
         this.rocketAmount = rocketAmount;
     }
-    
+
     public void notice(Scud enemyMissile) {
-        
+        lauchPatriot();
+    }
+
+    private void lauchPatriot() {
+        executor.execute(this);
+    }
+
+    @Override
+    public void run() {
+        for (int i= 0;i<rocketAmount;i++) {
+            battery.launchPatriot();
+        }
     }
     
     
+
 }
